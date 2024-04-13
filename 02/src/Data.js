81de1cd61953a73af711c45e62dd54354d592366
Vi042001 <vp222370@gmail.com>
2024-04-14 01:35:45 +0530

    usestate

--- 02/src/Data.js
+++ 02/src/Data.js
@@ -4,11 +4,40 @@ import datas from './datas.json';
 const Data = () => {
 
   const [nData,setnData] = useState(datas);
-  console.log(datas)
+
+  const handleRemove = (itemId) =>{
+    setnData(nData.filter(item =>item.id!==itemId))
+  }
+  const handleUpdate =(itemId) =>{
+    setnData(nData.map(item =>{
+      if (item.id === itemId) {
+        return{name:"newName"}
+        
+      }
+      else{
+        return item;
+      }
+    }))
+  }
+  
     return (
     <div>
-      
+    <ul>{
+     nData.map(item=>
+        <li key={item.id}>
+          {item.name}
+          <br/>
+         < button onClick={() =>handleUpdate(item.id)}>Update</button>
+          <button onClick={() =>handleRemove(item.id)}>Remove</button>
+
+        </li>
+     )
+      }
+    </ul>
+    <button onClick={() => setnData([])}>Clear</button>
     </div>
   )
 }
+
+
 export default Data
